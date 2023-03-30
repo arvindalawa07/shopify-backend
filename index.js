973ec71cd9d7e23a8368645b10c489258b9ba5c1
arvindalawa07 <arvindalawa1998@gmail.com>
2023-03-30 18:40:35 +0530

    testing

--- index.js
+++ index.js
@@ -46,51 +46,53 @@ app.use(
 );
 
 app.post("/", async (req, res) => {
-  console.log("request", req.locals.shopify.session);
-  console.log("response", res.locals.shopify.session);
-  console.log("cart id here", cart_id);
   try {
-    // const response = await fetch(
-    //   "https://ekartbook.myshopify.com/admin/api/2023-01/products.json",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       "X-Shopify-Access-Token": "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",
-    //     },
-    //     body: JSON.stringify(req.body),
-    //   }
-    // );
+    const response = await fetch(
+      "https://ekartbook.myshopify.com/admin/api/2023-01/products.json",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": "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",
+        },
+        body: JSON.stringify(req.body),
+      }
+    )
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+    console.log("request", req.locals.shopify.session);
+    console.log("response", res.locals.shopify.session);
+    console.log("cart id here", cart_id);
     // const addedData = await response.json();
     // console.log("added data", addedData);
     // const variantId = addedData.product.variants[0].id;
-    await fetch("https://ekartbook.myshopify.com/cart/add.json", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Cookie: `cart=${cart_id}`,
-        "X-Shopify-Access-Token": "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",
-      },
-      body: JSON.stringify({
-        items: [
-          {
-            id: 44643284582692,
-            properties: {
-              No_of_pages: 100,
-              binding_type: "spiral",
-              lines: 1,
-              cover_type: "soft",
-            },
-            quantity: 1,
-            variant_id: 44837600264484,
-          },
-        ],
-      }),
-    }).then((res) =>
-      res.json().then((result) => {
-        console.log("Result is here", result);
-      })
-    );
+    //   await fetch("https://ekartbook.myshopify.com/cart/add.json", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Cookie: `cart=${cart_id}`,
+    //       "X-Shopify-Access-Token": "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",
+    //     },
+    //     body: JSON.stringify({
+    //       items: [
+    //         {
+    //           id: 44643284582692,
+    //           properties: {
+    //             No_of_pages: 100,
+    //             binding_type: "spiral",
+    //             lines: 1,
+    //             cover_type: "soft",
+    //           },
+    //           quantity: 1,
+    //           variant_id: 44837600264484,
+    //         },
+    //       ],
+    //     }),
+    //   }).then((res) =>
+    //     res.json().then((result) => {
+    //       console.log("Result is here", result);
+    //     })
+    //   );
   } catch (error) {
     console.log("in catch block", error);
   }
