2be6783e5be958f7c93f4898d713a5266c17d51e
arvindalawa07 <arvindalawa1998@gmail.com>
2023-04-03 17:27:14 +0530

    testing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+const Client = require("shopify-buy");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-const PORT = 3500;
+const PORT = 8080;
 app.use(
   cors({
     origin: "*",
@@ -47,57 +48,90 @@ app.use(
 
 app.post("/", async (req, res) => {
   try {
-    const response = await fetch(
-      "https://ekartbook.myshopify.com/admin/api/2023-01/products.json",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Shopify-Access-Token": "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",
-        },
-        // body: JSON.stringify(req.body),
-      }
-    )
-      .then((result) => result.json())
-      .then((data) => console.log(data));
-    console.log("request", req.locals);
-    console.log("response", res.locals);
-    console.log("cart id here", cart_id);
-    // const addedData = await response.json();
-    // console.log("added data", addedData);
-    // const variantId = addedData.product.variants[0].id;
-    //   await fetch("https://ekartbook.myshopify.com/cart/add.json", {
+    // const response = await fetch(
+    //   "https://ekartbook.myshopify.com/admin/api/2023-01/products.json",
+    //   {
     //     method: "POST",
     //     headers: {
     //       "Content-Type": "application/json",
-    //       Cookie: `cart=${cart_id}`,
     //       "X-Shopify-Access-Token": "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",
     //     },
-    //     body: JSON.stringify({
-    //       items: [
-    //         {
-    //           id: 44643284582692,
-    //           properties: {
-    //             No_of_pages: 100,
-    //             binding_type: "spiral",
-    //             lines: 1,
-    //             cover_type: "soft",
-    //           },
-    //           quantity: 1,
-    //           variant_id: 44837600264484,
-    //         },
-    //       ],
-    //     }),
-    //   }).then((res) =>
-    //     res.json().then((result) => {
-    //       console.log("Result is here", result);
-    //     })
-    //   );
+    //     body: JSON.stringify(req.body),
+    //   }
+    // )
+    //   .then((result) => result.json())
+    //   .then((data) => console.log(data));
+    // console.log("request", req.locals);
+    // console.log("response", res.locals);
+    // console.log("cart id here", cart_id);
+    // const addedData = await response.json();
+    // console.log("added data", addedData);
+    // const variantId = addedData.product.variants[0].id;
+
+    await fetch("https://ekartbook.myshopify.com/cart/add.json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `cart=${cart_id}`,
+        // "X-Shopify-Storefront-Access-Token": "25b46aacf884ff262a21217205e1fa50",
+        "X-Shopify-Access-Token": "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",
+      },
+      body: JSON.stringify({
+        items: [
+          {
+            id: 44643284582692,
+            properties: {
+              No_of_pages: 100,
+              binding_type: "spiral",
+              lines: 1,
+              cover_type: "soft",
+            },
+            quantity: 10,
+            variant_id: 44643287105828,
+          },
+        ],
+      }),
+    }).then((res) =>
+      res.json().then((result) => {
+        console.log("Result is here", result);
+      })
+    );
   } catch (error) {
     console.log("in catch block", error);
   }
 });
 
+// const Shopify = require("shopify-api-node");
+
+// const shopify = new Shopify({
+//   shopName: "https://ekartbook.myshopify.com",
+//   apiKey: "01b09dbe8bc2c58a4930dd89caf91e67",
+//   password: "1d0915e0562954263bccc96b70edfe0f",
+// });
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+//   const { productId, title, price, quantity } = req.body;
+//   console.log(shopify.cart);
+//   shopify.cart
+//     .create({
+//       items: [
+//         {
+//           id: "44643287630116",
+//           // title,
+//           quantity: 4,
+//         },
+//       ],
+//     })
+//     .then((cart) => {
+//       res.json(cart);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res
+//         .status(500)
+//         .send("An error occurred while adding the product to the cart");
+//     });
 app.listen(PORT, () => {
   console.log("Server is running on ", PORT);
 });
