efa4f4fa11030f5db6e5bab1cd04ac8431f0f8b7
arvindalawa07 <arvindalawa1998@gmail.com>
2023-05-11 10:34:06 +0530

    testing

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ app.post("/cart/add", async (req, resp) => {
           position: 1,
           "width": 100,
           "height": 140,
-          src: image,
+          // src: image,
+          src:'https://www.google.com/url?sa=i&url=http%3A%2F%2Fclipart-library.com%2Ffree-book-image.html&psig=AOvVaw2RaaX8-oKRlVwC4a_tn2cW&ust=1683867807520000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPifiMm-7P4CFQAAAAAdAAAAABAE',
         }
       }),
     }
